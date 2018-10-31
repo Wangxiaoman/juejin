84240a5da69da94bc9d49354016f290dc3679884
王晓满 <wangxiaoman@ehomepay.com.cn>
2018-10-31 14:45:48 +0800

    init

---
+++ src/main/java/im/Application.java
@@ -0,0 +1,33 @@
+package im;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@SpringBootApplication
+@EnableAsync
+@EnableScheduling
+@EnableTransactionManagement
+public class Application{
+	 //根据需要初始化线程池
+    @Bean
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(50);
+        executor.setMaxPoolSize(100);
+        executor.setQueueCapacity(1000000);
+        return executor;
+    }
+    
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
